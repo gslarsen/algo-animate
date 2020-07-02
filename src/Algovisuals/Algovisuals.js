0e49d1804d35182b6b6067cf320f6be5439e3283
Greg Larsen <gslarsen2718@gmail.com>
2020-07-02 14:30:20 -0400

    initial

---
+++ src/Algovisuals/Algovisuals.js
@@ -0,0 +1,176 @@
+import React, { Component } from "react";
+
+import "./Algovisuals.css";
+import Node from "./Node/Node";
+import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
+
+const ANIMATION_TIME = 3;
+const START_NODE_ROW = 20;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 20;
+const FINISH_NODE_COL = 35;
+
+class Algovisuals extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
+
+  componentDidMount() {
+    const grid = getInitialGrid();
+    this.setState({ grid });
+  }
+
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseEnter(row, col) {
+    
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  handleMouseUp() {
+    this.setState({ mouseIsPressed: false });
+  }
+
+  handleClearBoard() {
+    const visitedNodes = document.querySelectorAll(".node-visited");
+    const pathNodes = document.querySelectorAll(".node-shortest-path");
+    const wallNodes = document.querySelectorAll(".node-wall");
+    const nodeStart = document.querySelector(`#node-${START_NODE_ROW}-${START_NODE_COL}`);
+    const nodeFinish = document.querySelector(`#node-${FINISH_NODE_ROW}-${FINISH_NODE_COL}`);
+
+    [].forEach.call(pathNodes, function (el) {
+      el.className = "node";
+    });
+
+    [].forEach.call(visitedNodes, function (el) {
+      el.className = "node";
+    });
+
+    [].forEach.call(wallNodes, function (el) {
+      el.className = "node";
+    });
+    nodeStart.className = "node node-start";
+    nodeFinish.className = "node node-finish";
+    this.setState({grid: getInitialGrid(), mouseIsPressed: false});
+  }
+
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, ANIMATION_TIME * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-visited";
+      }, ANIMATION_TIME * i);
+    }
+  }
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-shortest-path";
+      }, 50 * i);
+    }
+  }
+
+  visualizeDijkstra() {
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+  }
+
+  render() {
+    const { grid, mouseIsPressed } = this.state;
+
+    return (
+      <div>
+        <button id="reset" onClick={() => this.handleClearBoard()}>
+          Reset
+        </button>
+        <button onClick={() => this.visualizeDijkstra()}>Visualize</button>
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx} className="row">
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isFinish, isStart, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      mouseIsPressed={mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                      row={row}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 50; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(col, row));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
+
+export default Algovisuals;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Algovisuals from './Algovisuals/Algovisuals';
+
+function App() {
+  return (
+    <div className="App">
+      <Algovisuals />
+    </div>
+  );
+}
+
+export default App;
